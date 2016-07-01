70b1c0286c10e5abf274d9c7deb2e54fc2f5bce6
ondevelop <sernurb83@mail.ru>
2016-07-01 23:21:12 +0900

    runnable project created

---
+++ src/main/java/ykt/ios4miui3/gavrique/Main.java
@@ -0,0 +1,10 @@
+package ykt.ios4miui3.gavrique;
+
+/**
+ * Created by sergeystepanov on 01.07.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("server starting");
+    }
+}
